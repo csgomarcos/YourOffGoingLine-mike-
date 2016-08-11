a0138a75db229edb56090e2389f65faac6ba36c3
John Appleseed <mmills88888888@gmail.com>
2016-08-11 14:25:18 -0400

    Health and damage
    I p\put dem in da gem.

---
+++ Assets/Damage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+	public int shipHealth = 100;
+	public int collisionDamage = 50;
+	public HealthBar healthBar;
+
+	public void OnCollisionEnter2D(Collision2D collider) {
+		print ("hit object");
+		TakeDamage (collisionDamage);
+	}
+
+	public void TakeDamage(int damage) {
+		print ("take damaage");
+		shipHealth = shipHealth - damage;
+		healthBar.SetHealth (shipHealth);
+		if (shipHealth <= 0) {
+			healthBar.DeathScreen ();
+		}
+	}
+}
---
+++ Assets/HealthBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	public Text deathbox;
+	public Text textbox;
+	public void SetHealth(int health) {
+		textbox.text = "Health: " + health;
+	}
+	public void DeathScreen (){
+		deathbox.text = "GAME OVER";
+	}
+}
